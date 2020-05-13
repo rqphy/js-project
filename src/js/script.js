20fb515db880e36b9c4eeda826651076a55277ee
Raphael <raphael.ferreira@hetic.net>
2020-05-13 15:17:10 +0200

    add new pages + header links

--- src/js/script.js
+++ src/js/script.js
@@ -1,77 +1,180 @@
-// const $root = document.querySelector(".root");
-
-// function createDiv(message, color, font) {
-//   const newDiv = document.createElement("div");
-//   const newContent = document.createTextNode(message);
-//   newDiv.appendChild(newContent);
-//   newDiv.classList.add(color);
-//   newDiv.classList.add(font);
-//   $root.appendChild(newDiv);
-// }
-
-// createDiv("Hello World!", "red", "Open-Sans");
 document.addEventListener("DOMContentLoaded", function () {
   class Model {
     constructor() {
       this.pages = [
         {
           title: "Homepage",
-          url: "/",
+          url: "#",
           background: "red",
+          content: `
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+					`,
+          dynamisme: () => {
+            const li = document.querySelectorAll("li");
+
+            function toUpper(element) {
+              element.innerText = element.innerText.toUpperCase();
+            }
+
+            li.forEach((element) => {
+              toUpper(element);
+            });
+          },
         },
         {
           title: "Contact",
-          url: "/contact",
+          url: "#contact",
           background: "green",
+          form: true,
         },
         {
-          title: "About",
-          url: "/about",
-          background: "yellow",
-          image: true,
-          image_src: "assets/javascript.png",
-          // probleme entre parcel et le src
+          title: "Test",
+          url: "#test",
+          background: "purple",
+          content: `
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+						<div class="parallax-container">
+							<div class="parallax">
+
+							</div>
+						</div>
+						<div>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas nostrum quidem sequi aperiam numquam minima possimus? Atque possimus molestiae libero, voluptas quidem architecto facilis vero delectus nihil provident tempora velit.
+							<ul>
+								<li>lorem ipsum</li>
+								<li>dolor sit</li>
+								<li>amet consectetur</li>
+							</ul>
+						</div>
+					`,
+          dynamisme: () => {
+            window.addEventListener("scroll", (e) => {
+              let scrollTop = e.target.scrollingElement.scrollTop;
+
+              console.log(scrollTop);
+
+              document.querySelector(
+                ".parallax"
+              ).style.backgroundPosition = `center calc(50% - ${
+                scrollTop / 2
+              }px)`;
+            });
+          },
+        },
+        {
+          title: "Test2",
+          url: "#test2",
+          background: "cyan",
+          content: `
+          <section class="hero">
+            <div class="hero__content">
+              <h2 class="hero__title>Let's try something</h2>
+              <p class="hero__description">Here goes the description</p>
+            </div>
+          </section>
+          `,
         },
       ];
     }
 
     getPageByUrl(url) {
-      return this.pages.find((page) => page.url === url);
+      return this.pages.find((page) => page.url == url);
     }
   }
 
   class View {
-    constructor(container) {
-      this.container = document.querySelector(`.${container}`);
+    constructor(pages) {
+      this.container = document.querySelector(".container");
+      this.container.innerHTML = "";
+      this.addHeader(pages);
     }
 
-    changeTitle(word) {
-      this.container.innerHTML = word;
+    run(dynamisme) {
+      dynamisme();
+    }
+
+    addContent(content) {
+      const contentContainer = document.createElement("div");
+      contentContainer.classList.add("content");
+      contentContainer.innerHTML = content;
+
+      this.container.appendChild(contentContainer);
+    }
+
+    addHeader(pages) {
+      pages.forEach((page) => {
+        const button = document.createElement("button");
+
+        button.innerText = page.title;
+
+        this.container.appendChild(button);
+
+        button.addEventListener("click", () => {
+          location.hash = page.url;
+        });
+      });
+    }
+
+    changeTitle(text) {
+      const title = document.createElement("h1");
+
+      title.innerText = text;
+
+      this.container.appendChild(title);
     }
 
     changeBackground(color) {
       this.container.style.background = color;
     }
 
-    loadImage(image_src) {
-      const image = document.createElement("img");
-      image.setAttribute("src", image_src);
-      this.container.appendChild(image);
+    addForm() {
+      let input = document.createElement("input");
+      input.setAttribute("type", "text");
+      input.classList.add("input-text");
+
+      this.container.appendChild(input);
     }
   }
 
   function controller() {
-    const data = new Model();
-    const currentPage = data.getPageByUrl("/about");
+    let data = new Model();
 
-    const page = new View("root");
+    const currentPage = data.getPageByUrl(location.hash || "#");
+
+    let page = new View(data.pages);
     page.changeTitle(currentPage.title);
     page.changeBackground(currentPage.background);
 
-    if (currentPage.image) {
-      page.loadImage(currentPage.image_src);
+    if (currentPage.content) {
+      page.addContent(currentPage.content);
+    }
+
+    if (typeof currentPage.dynamisme === "function") {
+      page.run(currentPage.dynamisme);
+    }
+
+    if (currentPage.form) {
+      page.addForm();
     }
   }
+
+  window.addEventListener("hashchange", () => {
+    controller();
+  });
 
   controller();
 });
